8fdbf611669b24cc178bfd7b18eadcc1923260ad
Heiko Jakubzik <heiko.jakubzik@shj-online.de>
2025-11-05 19:32:16 +0100

    Initial upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# SUV
+
+A cli to manage data for supervisions of theses: dates, discussion notes, outlines, contact information, reports.
+
+More info to follow ...
---
+++ src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+struct SUV_FOLDER {
+    main_directory: String,
+    archive_directory: String,
+}
+
+struct Student {
+    last_name: String,
+    first_name: String,
+    email: String,
+}
+
+struct Thesis {
+    student: Student,
+    title: String,
+}
